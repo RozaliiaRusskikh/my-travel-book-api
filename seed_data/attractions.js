65eec3bdecb9d5bbe2e6933b3b708dfab564a629
Roza Russkikh <rrrusskikh@gmail.com>
2023-04-09 10:55:46 -0700

    Change path

--- seed_data/attractions.js
+++ seed_data/attractions.js
@@ -5,7 +5,7 @@ module.exports = [
     post_id: 1,
     description:
       "Petra is a historic city located in southern Jordan, known for its stunning rock-cut architecture and fascinating history. During my visit, I was in awe of the city's unique beauty and the stories it held within its walls.I started my journey by exploring the Siq, a narrow canyon that serves as the entrance to the city. The Siq is lined with towering rock walls and leads to the famous Treasury, one of Petra's most iconic structures.",
-    image_path: "http://localhost:8080/treasury.jpg",
+    image_path: "https://my-travel-book.herokuapp.com/treasury.jpg",
   },
 
   {
@@ -14,7 +14,7 @@ module.exports = [
     post_id: 1,
     description:
       "Next, I ventured deeper into the city and discovered its many hidden treasures. The Monastery, another impressive rock-cut structure, offers stunning views of the surrounding landscape. I also learned about the Nabataean people, who built Petra over 2,000 years ago. They were skilled traders and architects who developed a complex water management system that allowed them to thrive in the arid desert environment. The city was an important stop on the ancient trade routes that linked Arabia, Egypt, and Syria. Despite its ancient history, Petra remains a vibrant and living city. Visitors can interact with local Bedouin people who still call the city home, and experience their traditional hospitality and culture. Overall, Petra is a unique and awe-inspiring destination that offers a glimpse into the rich history and culture of the ancient Middle East.",
-    image_path: "http://localhost:8080/monastery.jpg",
+    image_path: "https://my-travel-book.herokuapp.com/monastery.jpg",
   },
   {
     id: 3,
@@ -22,7 +22,7 @@ module.exports = [
     post_id: 1,
     description:
       "Wadi Rum is a vast desert valley located in the southern part of Jordan. This unique destination is known for its stunning natural beauty, rugged landscape, and rich history. During my visit, I was captivated by the unique blend of colors, textures, and shapes that define this mesmerizing desert. I started my journey by taking a jeep tour through the desert, which allowed me to experience the raw beauty of the landscape up close. The towering rock formations, sand dunes, and narrow canyons were unlike anything I had ever seen before. I also had the opportunity to interact with the local Bedouin people, who have been living in the area for centuries.",
-    image_path: "http://localhost:8080/wadi-rum.jpg",
+    image_path: "https://my-travel-book.herokuapp.com/wadi-rum.jpg",
   },
   {
     id: 4,
@@ -30,7 +30,7 @@ module.exports = [
     post_id: 1,
     description:
       "The Dead Sea is a natural wonder located between Jordan and Israel. It is the lowest point on Earth and is famous for its high salt concentration, which makes it impossible for most living things to survive in its waters. However, this unique environment has also been found to have healing properties, making it a popular destination for wellness and relaxation. During my visit to the Dead Sea, I first experienced the unusual sensation of floating effortlessly on the water's surface due to its high salt concentration. The sea's mineral-rich mud is also known for its therapeutic properties, and I indulged in a mud bath that left my skin feeling soft and rejuvenated.",
-    image_path: "http://localhost:8080/dead-sea.jpg",
+    image_path: "https://my-travel-book.herokuapp.com/dead-sea.jpg",
   },
   {
     id: 5,
@@ -38,7 +38,7 @@ module.exports = [
     post_id: 2,
     description:
       "The Caucasus region is home to some of the world's most stunning landscapes and natural wonders, including the two rivers that flow through its heart: Aragva and Kura. Each of these rivers has a unique history and significance that have shaped the region's culture and way of life.",
-    image_path: "http://localhost:8080/aragva.jpg",
+    image_path: "https://my-travel-book.herokuapp.com/aragva.jpg",
   },
   {
     id: 6,
@@ -46,7 +46,7 @@ module.exports = [
     post_id: 3,
     description:
       "The Colosseum, located in the heart of Rome, is one of the most iconic landmarks in the world and a symbol of the ancient Roman Empire. This magnificent amphitheater was built almost 2,000 years ago and was the largest of its kind in the ancient world, capable of holding up to 80,000 spectators. It was used for a variety of events, including gladiator contests, animal hunts, and public spectacles. Today, visitors can explore the Colosseum and learn about its history through interactive exhibits and guided tours. With its impressive architecture and fascinating history, the Colosseum is a must-visit destination for anyone traveling to Rome.",
-    image_path: "http://localhost:8080/colosseum.jpg",
+    image_path: "https://my-travel-book.herokuapp.com/colosseum.jpg",
   },
   {
     id: 7,
@@ -54,7 +54,7 @@ module.exports = [
     post_id: 3,
     description:
       "The Pantheon is one of the most impressive and well-preserved ancient buildings in Rome, dating back to the 2nd century AD. This incredible feat of architecture was designed as a temple to all the gods, and its massive dome remains the largest unsupported dome in the world. The interior is adorned with intricate marble floors, stunning frescoes, and ancient statues, all of which create a sense of awe and wonder. Despite being over 2,000 years old, the Pantheon still stands as a testament to the engineering and architectural achievements of ancient Rome. Today, it continues to be a popular tourist destination, attracting visitors from all over the world who come to marvel at its beauty and history.",
-    image_path: "http://localhost:8080/pantheon.jpg",
+    image_path: "https://my-travel-book.herokuapp.com/pantheon.jpg",
   },
   {
     id: 8,
@@ -62,7 +62,7 @@ module.exports = [
     post_id: 3,
     description:
       "The Vatican City is one of the smallest and most fascinating countries in the world. It is the spiritual center of the Catholic Church and home to some of the most famous and iconic art and architecture in human history. The Vatican Museums are a must-see for any visitor, with stunning collections of art, sculpture, and ancient artifacts. The Sistine Chapel, with its breathtaking frescoes by Michelangelo, is one of the most famous and iconic works of art in the world. The Vatican is also home to St. Peter's Basilica, one of the largest and most impressive churches in the world, which boasts a stunning array of artwork and architecture. Visitors can explore the Vatican Gardens, the Apostolic Palace, and the numerous museums and galleries that make up this unique and fascinating country. The Vatican is not only a cultural and historical treasure, but also a living and vibrant city with a rich spiritual and cultural heritage.",
-    image_path: "http://localhost:8080/vatican.jpg",
+    image_path: "https://my-travel-book.herokuapp.com/vatican.jpg",
   },
   {
     id: 9,
@@ -70,6 +70,6 @@ module.exports = [
     post_id: 4,
     description:
       "Tulum is a stunning archaeological site located on the eastern coast of the Yucatan Peninsula in Mexico. It was one of the last cities built by the ancient Maya civilization, and served as an important trading port for centuries. Today, visitors can explore the ruins of Tulum and marvel at the impressive stone structures, including the iconic Temple of the Frescoes, the Castle, and the Temple of the Descending God. The site is perched on a cliff overlooking the turquoise waters of the Caribbean Sea, providing breathtaking views of the coastline. In addition to its historical significance, Tulum is also known for its beautiful beaches and lush natural surroundings, making it a popular destination for both history buffs and nature lovers. Visitors can swim in the crystal-clear waters, snorkel among colorful fish and coral reefs, or simply relax on the white sand beaches. Tulum is a true gem of the ancient world, and a must-see destination for anyone interested in history, archaeology, or natural beauty.",
-    image_path: "http://localhost:8080/tulum.jpg",
+    image_path: "https://my-travel-book.herokuapp.com/tulum.jpg",
   },
 ];
