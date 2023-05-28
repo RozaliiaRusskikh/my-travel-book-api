f9a3679ace7615342209ae3b0e6ad8aee6d7afe3
Roza Russkikh <rrrusskikh@gmail.com>
2023-05-27 18:08:26 -0700

    Solve CORS problem

--- server.js
+++ server.js
@@ -4,14 +4,14 @@ const app = express();
 
 const cors = require("cors");
 
-require("dotenv").config({path: ".env"});
+require("dotenv").config();
 let { CLIENT_URL, PORT } = process.env;
 PORT = PORT || 5050;
 
 const postsRoutes = require("./routes/postsRoute");
 
 //middleware
-app.use(cors({ origin: CLIENT_URL }));
+app.use(cors());
 app.use(express.json());
 app.use(express.static("./public/images"));
 
