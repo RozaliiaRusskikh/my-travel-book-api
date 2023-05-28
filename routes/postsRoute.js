98a3e87400afeac820e3da6b65bd5f5b9a05edd3
Roza Russkikh <rrrusskikh@gmail.com>
2023-05-27 18:02:07 -0700

    Fallback

--- routes/postsRoute.js
+++ routes/postsRoute.js
@@ -1,42 +1,17 @@
 const router = require("express").Router();
 const postsController = require("../controllers/postsController");
 const multer = require("multer");
-const aws = require("aws-sdk");
-const multerS3 = require("multer-s3");
 
-require("dotenv").config();
-let { SECRET_ACCESS_KEY, ACCESS_KEY_ID } = process.env;
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "public/images/");
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  },
+});
 
-// aws.config.update({
-//   secretAccessKey: SECRET_ACCESS_KEY,
-//   accessKeyId: ACCESS_KEY_ID,
-//   region: "us-west-1",
-// });
-
-// const s3 = new aws.S3();
-
-// const upload = multer({
-//   storage: multerS3({
-//     s3: s3,
-//     acl: "public-read",
-//     bucket: "my-travel-memory-book",
-//     key: function (req, file, cb) {
-//       cb(null, file.originalname);
-//     },
-//   }),
-// });
-
-//** Static files storage **
-// const storage = multer.diskStorage({
-//   destination: function (req, file, cb) {
-//     cb(null, "public/images/");
-//   },
-//   filename: function (req, file, cb) {
-//     cb(null, file.originalname);
-//   },
-// });
-
-// const upload = multer({ storage: storage });
+const upload = multer({ storage: storage });
 
 router
   .route("/")
