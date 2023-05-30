18ae29854a759f94581dc2460b3d399299c342da
Roza Russkikh <rrrusskikh@gmail.com>
2023-05-30 14:16:18 -0700

    Fix cors

--- server.js
+++ server.js
@@ -7,10 +7,11 @@ require("dotenv").config();
 let { PORT, CLIENT_URL } = process.env;
 PORT = PORT || 5050;
 
+app.use(cors());
+
 const postsRoutes = require("./routes/postsRoute");
 
 //middleware
-app.use(cors());
 app.use(express.json());
 app.use(express.static("./public/images"));
 
