69c2b4d3805c68c28a84305e3bef8c877f94083c
Roza Russkikh <rrrusskikh@gmail.com>
2023-05-30 14:13:19 -0700

    Fix cors

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ PORT = PORT || 5050;
 const postsRoutes = require("./routes/postsRoute");
 
 //middleware
-app.use(cors({ origin: "*" }));
+app.use(cors());
 app.use(express.json());
 app.use(express.static("./public/images"));
 
